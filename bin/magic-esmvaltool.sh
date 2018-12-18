aae11e77a434eee8b4a4d3c449a1cfed0d8a28b8
Niels Drost <n.drost@esciencecenter.nl>
2018-12-18 20:06:26 +0000

    added config-user.yml for CP4CDS

--- bin/magic-esmvaltool.sh
+++ bin/magic-esmvaltool.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -exo pipefail
 
-export PATH="/group_workspaces/jasmin2/cp4cds1/magic-test/conda/bin:$PATH"
-. activate esmvaltool3 
+export PATH="/group_workspaces/jasmin2/cp4cds1/magic-demo/conda/bin:$PATH"
+. activate esmvaltool
 
 # User configuration file
-config=/group_workspaces/jasmin2/cp4cds1/magic-test/config-user.yml
+config=/home/users/ndrost/c3s-magic-cylc-suite/config-user.yml
 
 echo Running recipe $1
 
 esmvaltool -c $config $1
+
